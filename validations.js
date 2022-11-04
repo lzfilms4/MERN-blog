7fd67e6ae566395fb26548114f5f34c9d0dc8938
lzfilms4 <lzfilms4@gmail.com>
2022-11-04 22:06:39 +0700

    Update validations.js

--- validations.js
+++ validations.js
@@ -1,20 +1,24 @@
 const { body } = require('express-validator');
-
-export const loginValidation = [
+const loginValidation = [
   body('email', 'Неверный формат почты').isEmail(),
   body('password', 'Пароль должен быть минимум 5 символов').isLength({ min: 5 }),
 ];
 
-export const registerValidation = [
+const registerValidation = [
   body('email', 'Неверный формат почты').isEmail(),
   body('password', 'Пароль должен быть минимум 5 символов').isLength({ min: 5 }),
   body('fullName', 'Укажите имя').isLength({ min: 3 }),
   body('avatarUrl', 'Неверная ссылка на аватарку').optional().isURL(),
 ];
 
-export const postCreateValidation = [
+const postCreateValidation = [
   body('title', 'Введите заголовок статьи').isLength({ min: 3 }).isString(),
   body('text', 'Введите текст статьи').isLength({ min: 3 }).isString(),
   body('tags', 'Неверный формат тэгов').optional().isString(),
   body('imageUrl', 'Неверная ссылка на изображение').optional().isString(),
 ];
+module.exports = {
+  loginValidation,
+  registerValidation,
+  postCreateValidation,
+};
